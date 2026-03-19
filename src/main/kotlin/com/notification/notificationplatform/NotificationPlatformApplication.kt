e14d2b06936f39871697f9c29c5305e02988dfbb
L-a-z-e <yysi8771@gmail.com>
2026-03-19 22:41:42 +0900

    chore: init notification-platform project
    Spring Boot 4.0 + Kotlin 기반 알림 플랫폼 초기 프로젝트 구조

---
+++ src/main/kotlin/com/notification/notificationplatform/NotificationPlatformApplication.kt
@@ -0,0 +1,11 @@
+package com.notification.notificationplatform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationPlatformApplication
+
+fun main(args: Array<String>) {
+    runApplication<NotificationPlatformApplication>(*args)
+}
---
+++ src/test/kotlin/com/notification/notificationplatform/NotificationPlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.notification.notificationplatform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotificationPlatformApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
